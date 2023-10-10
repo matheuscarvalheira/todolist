11989d28986c7081b72557d2cdd2cd508bd99318
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2023-10-10 12:13:03 -0300

    first commit

---
+++ src/test/java/br/com/matheuscarvalheira/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheuscarvalheira.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
